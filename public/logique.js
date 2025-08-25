31f07f3d856683258260319cc94d7e32a0c36110
tshoney <lucaslolo1312@gmail.com>
2025-08-25 03:06:30 +0200

    test

--- public/logique.js
+++ public/logique.js
@@ -9,33 +9,6 @@ const blockchain = 'solana';
 let currentCount = 0;
 let marketcap = 0;
 
-/*async function fetchMarketCap() {
-  try {
-    const response = await fetch(`https://api.dexscreener.com/latest/dex/pairs/${blockchain}/${tokenAddress}`);
-    if (!response.ok) throw new Error('Erreur API DexScreener');
-
-    const dataResp = await response.json();
-
-    // Si la donnée existe
-    marketcap = dataResp?.pairs?.[0]?.marketCap || 100000000;
-    //clicksPerSecond = marketcap / secondsInDay;
-
-    // Mise à jour du DOM
-    if (marketCapEl) {
-      marketCapEl.textContent = `Marketcap: $${marketcap.toLocaleString()}`;
-    }
-
-    console.log('Marketcap:', marketcap);
-
-  } catch (error) {
-    console.error('Erreur lors de la récupération du market cap:', error);
-  }
-}*/
-
-// Lance la récupération du Market Cap et mise à jour auto toutes les 30 sec
-//fetchMarketCap();
-//setInterval(fetchMarketCap, 30000);
-
 
 async function fetchClickPerMinute() {
   try {
@@ -70,6 +43,21 @@ fetchClickPerMinute();
 setInterval(fetchClickPerMinute, 30000);
 
 
+async function fetchCount() {
+  try {
+    console.log('Fetching count from server...');
+    const res = await fetch('/.netlify/functions/counter');
+    console.log('Response status:', res.status);
+    if (!res.ok) throw new Error('Network response not ok');
+
+    const data = await res.json();
+    console.log('Data received:', data);
+    return data.dailyCount;
+  } catch (err) {
+    console.error('fetchCount error:', err);
+    return currentCount;
+  }
+}
 
 function animateCount(target) {
   const step = () => {
