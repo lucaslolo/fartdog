3e31c84fe9330e962d549c9ce136861f1adae6b5
tshoney <lucaslolo1312@gmail.com>
2025-08-25 03:00:00 +0200

    test

--- public/logique.js
+++ public/logique.js
@@ -1,12 +1,15 @@
 const countEl = document.getElementById('count');
 const marketCapEl = document.getElementById('marketcap');
+const clickPerSecondeEl = document.getElementById('clickperseconde');
+const clickPerMinuteEl = document.getElementById('clickperminute');
+
 const tokenAddress = 'EmidmqwsaEHV2qunR3brnQTyvWS9q7BM8CXyW9NmPrd';
 const blockchain = 'solana';
 
 let currentCount = 0;
 let marketcap = 0;
 
-async function fetchMarketCap() {
+/*async function fetchMarketCap() {
   try {
     const response = await fetch(`https://api.dexscreener.com/latest/dex/pairs/${blockchain}/${tokenAddress}`);
     if (!response.ok) throw new Error('Erreur API DexScreener');
@@ -27,11 +30,46 @@ async function fetchMarketCap() {
   } catch (error) {
     console.error('Erreur lors de la récupération du market cap:', error);
   }
+}*/
+
+// Lance la récupération du Market Cap et mise à jour auto toutes les 30 sec
+fetchMarketCap();
+setInterval(fetchMarketCap, 30000);
+
+
+async function fetchClickPerMinute() {
+  try {
+    const response = await fetch(`https://api.dexscreener.com/latest/dex/pairs/${blockchain}/${tokenAddress}`);
+    if (!response.ok) throw new Error('Erreur API DexScreener');
+
+    const dataResp = await response.json();
+
+    // Si la donnée existe
+    marketcap = dataResp?.pairs?.[0]?.marketCap || 100000000;
+    clicksPerSecond = marketcap / secondsInDay;
+    clicksPerMinute = clicksPerSecond * 60;
+
+    // Mise à jour du DOM
+    if (marketCapEl) {
+      marketCapEl.textContent = `Marketcap: $${marketcap.toLocaleString()}`;
+    }
+    if (clickPerSecondeEl) {
+      clickPerSecondeEl.textContent = `Clicks per second: ${clicksPerSecond.toLocaleString()}`;
+    }
+    if (clickPerMinuteEl) {
+      clickPerMinuteEl.textContent = `Clicks per minute: ${clicksPerMinute.toLocaleString()}`;
+    }
+
+  } catch (error) {
+    console.error('Erreur lors de la récupération du market cap:', error);
+  }
 }
 
 // Lance la récupération du Market Cap et mise à jour auto toutes les 30 sec
 fetchMarketCap();
 setInterval(fetchMarketCap, 30000);
+
+
 
 function animateCount(target) {
   const step = () => {
